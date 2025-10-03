cee20805c911512cb85eade63e6916e1de20a880
chandanv1010 <tuannc.dev@gmail.com>
2025-10-03 15:01:35 +0700

    Fix Component

--- app/View/Components/ibox.php
+++ app/View/Components/ibox.php
@@ -21,6 +21,6 @@ class ibox extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.backend.ibox');
+        return view('components.ibox');
     }
 }
